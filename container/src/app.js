bc2e881473746a57793fb9869cf6e003392dfbe8
RajaDey <raja.d@fnp.com>
2022-03-04 01:15:28 +0530

    Deployment script

--- container/src/app.js
+++ container/src/app.js
@@ -10,7 +10,7 @@ const App = () => {
 
   return (
     <>
-      <h1>RajaDey from Containersssss</h1>
+      <h1>RajaDey from Containersssssss</h1>
       <hr />
       <div ref={ref}></div>
     </>
