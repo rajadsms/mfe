9ffe76915007a5d5f013e11d99c16cefe8df82ee
RajaDey <raja.d@fnp.com>
2022-03-03 09:30:54 +0530

    Added github CI CD and prod config

---
+++ packages/container/config/webpack.prod.js
@@ -0,0 +1,23 @@
+const { merge } = require("webpack-merge");
+const commonConfig = require("./webpack.common");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const packageJson = require("../package.json");
+
+const domain = process.env.PROD_DOMAIN;
+const prodConfig = {
+  mode: "production",
+  output: {
+    filename: "[name].[contenthash].js",
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "container", //not use but good to use
+      remotes: {
+        marketingAlias: `marketingGloabalVar@${domain}/marketing/remoteentry.js`,
+      },
+      shared: packageJson.dependencies,
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, prodConfig);
--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -1,6 +1,5 @@
 const { merge } = require("webpack-merge");
 const commonConfig = require("./webpack.common");
-const htmlWebpackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const packageJson = require("../package.json");
 
@@ -20,9 +19,6 @@ const devConfig = {
         "./MarketingApp": "./src/bootstrap",
       },
       shared: packageJson.dependencies,
-    }),
-    new htmlWebpackPlugin({
-      template: "./public/index.html",
     }),
   ],
 };
