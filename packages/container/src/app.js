0198d73283615e5a60da95eb0aef3258dd4350b0
RajaDey <raja.d@fnp.com>
2022-03-08 00:40:57 +0530

    aws yaml file setup

--- packages/container/src/app.js
+++ packages/container/src/app.js
@@ -10,7 +10,7 @@ const App = () => {
 
   return (
     <>
-      <h1>RajaDey from Containersssssssssssssssssssssss</h1>
+      <h1>RajaDey from Containerssssssssssssssssssssssss</h1>
       <hr />
       <div ref={ref}></div>
     </>
